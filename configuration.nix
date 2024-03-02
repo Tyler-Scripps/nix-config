f9925728c90d5726ab23d1b9195f1b8dc2112b1e
Tyler Scripps <tylerscripps@gmail.com>
2024-03-02 09:39:53 -0700

    doesn;t actually configure firefox

--- configuration.nix
+++ configuration.nix
@@ -142,18 +142,6 @@ in
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # may or may not work (doesn't seem to)
-  programs.firefox.policies = {
-    DisablePocket = true;
-    ExtensionSettings = {
-      # Bitwarden:
-      "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
-        install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
-        installation_mode = "normal_installed";
-      };
-    };
-  };
-
   # mysql
   services.mysql = {
     enable = true;
