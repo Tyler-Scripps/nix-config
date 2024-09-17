e9428cadc1d38d24178f8ba9a75a20bb2db0fa3b
Tyler Scripps <tylerscripps@gmail.com>
2024-09-17 13:48:58 -0600

    new desktop.nix  for amd gpu

--- desktop.nix
+++ desktop.nix
@@ -1,67 +1,11 @@
 { config, lib, pkgs, ... }:
 {
 
-  boot.kernelParams = [
-    "video=card0-DP-1:2560x1440@165"
-    "video=card0-DP-2:2560x1440@165"
-    "video=card0-HDMI-A-1:2560x1440@165"
-    "video=card1-DP-4:1920x1080@60"
-    "video=card1-DP-5:1920x1080@60"
-    "video=card1-HDMI-A-2:1920x1080@60"
-  ];
-
-  # Enable OpenGL
-  hardware.opengl = {
-    enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
-  };
-
-  boot.initrd.kernelModules = [ "amdgpu" ];
-
-  # Load nvidia driver for Xorg and Wayland
-  services.xserver.videoDrivers = ["amdgpu nvidia"]; # or "nvidiaLegacy470 etc.
-
-  hardware.nvidia = {
-
-    # Modesetting is required.
-    modesetting.enable = true;
-
-    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    # Enable this if you have graphical corruption issues or application crashes after waking
-    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
-    # of just the bare essentials.
-    powerManagement.enable = false;
-
-    # Fine-grained power management. Turns off GPU when not in use.
-    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = false;
-
-    # Use the NVidia open source kernel module (not to be confused with the
-    # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of 
-    # supported GPUs is at: 
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
-    # Only available from driver 515.43.04+
-    # Currently alpha-quality/buggy, so false is currently the recommended setting.
-    open = false;
-
-    # Enable the Nvidia settings menu,
-	# accessible via `nvidia-settings`.
-    nvidiaSettings = true;
-
-    # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
-  };
-
-  hardware.openrazer.enable = true; 
+  hardware.openrazer.enable = true;
 
   environment.systemPackages = [
     pkgs.razergenie
     pkgs.openrazer-daemon
-    pkgs.nvtop
+    pkgs.polychromatic
   ];
-
-  # fixes electron apps with nvidia driver
-  environment.sessionVariables.NIXOS_OZONE_WL = "1";
 }
